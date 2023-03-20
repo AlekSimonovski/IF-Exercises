6651b8a4bc06d59517be484675b8d38030245a1d
unknown <alek.simonovski02@gmail.com>
2023-03-20 11:34:22 +0100

    Adding files to repository

---
+++ src/vezba2/vezba.java
@@ -0,0 +1,24 @@
+package vezba2;
+
+import java.util.Scanner;
+
+public class vezba {
+
+	public static void main(String[] args) {
+		boolean p, q;
+		Scanner tastatura = new Scanner(System.in);
+		p = tastatura.nextBoolean();
+		q = tastatura.nextBoolean();
+		if(p && q) {
+			q=false;
+		}else {
+			if(!q) {
+				System.out.println(p);
+			}
+			if(p==q) {
+				System.out.println(p || q);
+			}
+			System.out.println(q);	
+	}
+}
+}
